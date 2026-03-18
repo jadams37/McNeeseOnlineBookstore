6cb151f50c63424259ba4d9fda08fa2dd76ebb54
jadams37 <jma.personal@outlook.com>
2026-03-18 12:43:49 -0500

    Added documentation for js file

--- scripts/signup-next-form.js
+++ scripts/signup-next-form.js
@@ -1,4 +1,5 @@
 function nextForm() {
+    /* New form input and labels to shown on the next page of the Signup */
     const newFormInput=`<label for="name" class="name-label">Full Name</label><br>
                     <input class="name-input" id="name" type="text"><br>
                     <label for="address" class="address-label">Address</label><br>
@@ -6,15 +7,20 @@ function nextForm() {
                     <label for="phone" class="phone-label">Phone Number</label><br>
                     <input class="phone-input" id="password" type="text"><br>`;
 
+    /* Grabs the Signup form and the button used to proceed/submit the form */
     const form = document.getElementById('signup');
     const button = document.getElementById('next');
 
+    /* Iterates through each element contained in the signup form and 
+       sets their hidden attribute to true */
     document.querySelectorAll('#signup > *').forEach(element => {
         element.hidden = true;
     })
 
+    /* Append the new form inputs after hiding the previous */
     form.innerHTML += newFormInput;
 
+    /* Update the 'Next' button to become a submit button */
     button.textContent = "Register";
     button.type = "submit";
     button.onclick = "";
