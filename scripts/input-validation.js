698536149986089b997c09a79b98e10d83db0508
jadams37 <jma.personal@outlook.com>
2026-03-25 14:58:27 -0500

    Changed font for error messages

---
+++ scripts/input-validation.js
@@ -0,0 +1,8 @@
+function validateForm() {
+    const form = document.getElementById('signup');
+
+    const email = document.getElementById('email');
+    const username = document.getElementById('username');
+    const password = document.getElementById
+
+}
