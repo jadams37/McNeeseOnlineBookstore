a4e53e7ef688810cfcd8b61ddb741c173cb7a60c
JordanA2003 <jma.personal@outlook.com>
2026-03-26 15:47:29 -0500

    Removed unnecessary script file

--- scripts/input-validation.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    const form = document.getElementById('signup');
-
-    const email = document.getElementById('email');
-    const username = document.getElementById('username');
-    const password = document.getElementById
-
-}
