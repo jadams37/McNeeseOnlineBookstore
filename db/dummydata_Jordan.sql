9008fd6f5231c28a59d25e245f44201475e5df66
jadams37 <jma.personal@outlook.com>
2026-04-02 13:32:52 -0500

    Sample data for progress check

---
+++ db/dummydata_Jordan.sql
@@ -0,0 +1,5 @@
+INSERT INTO product(sku, isbn, title, author, publisher, condition, price)
+VALUES('0a000', '12112312323', 'TestBook', 'TestAuthor', 'TestPublisher', 'new', 234.99);
+
+INSERT INTO product(sku, isbn, title, author, condition, price)
+VALUES('0a001', '12112312324', 'ProductTest', 'ProductBrandTest', 'new', 10.99);
